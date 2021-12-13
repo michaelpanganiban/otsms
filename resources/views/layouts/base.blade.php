7278c511cd36d0405afd1ed10afd2db4d8f3b99f
John Michael Panganiban <johnmichaelpanganiban.its@gmail.com>
2021-12-13 09:05:49 +0800

    Add logo

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -158,7 +158,7 @@
             </nav>
             <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <a href="/" class="brand-link">
-                <img src="{{ asset('assets/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+                <img src="{{ asset('assets/images/logo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">OTSMS</span>
             </a>
             <div class="sidebar">
