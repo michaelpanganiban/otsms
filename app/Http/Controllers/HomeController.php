01a77eca599a63ae0e2f297fc4225cad8c0cb117
John Michael Panganiban <johnmichaelpanganiban.its@gmail.com>
2021-12-15 13:26:11 +0800

    Fix on error by login

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -23,9 +25,12 @@ class HomeController extends Controller
      */
     public function index()
     {
-        if(\Auth::user()->user_type === 0)
+        if(Auth::user()->user_type === 0)
             return view('welcome');
-        else
-            return view('home');
+        else{
+            DB::statement("SET SQL_MODE=''");
+        $data = DB::select("SELECT SUM(p.amount) as amount FROM product_sales p LEFT JOIN orders o ON p.product_id = o.product_id AND o.status NOT IN('Pending', 'Disapproved') WHERE MONTH(o.created_at) = MONTH(CURRENT_DATE())");
+            return view('home', compact('data'));
+        }
     }
 }
